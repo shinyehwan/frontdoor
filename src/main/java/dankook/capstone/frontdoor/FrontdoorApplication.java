d8e6df85248c1316ec761f4d235c67222544f8ff
shinyehwan <jghisjl007@naver.com>
2022-04-24 19:42:47 +0900

    initial commit

---
+++ src/main/java/dankook/capstone/frontdoor/FrontdoorApplication.java
@@ -0,0 +1,13 @@
+package dankook.capstone.frontdoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontdoorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontdoorApplication.class, args);
+	}
+
+}
